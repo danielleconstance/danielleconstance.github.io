75db8f871fbb2750898148b703da117f9b3d66b4
danielle <danielleconstance3@gmail.com>
2018-10-16 20:50:51 -0700

    initial comit

---
+++ JS/finalproject.js
@@ -0,0 +1,13 @@
+function stext() {
+    var x = document.getElementById("div1");
+    var y = document.getElementById("div2");
+    x.style.color = 'yellow';
+    y.style.color = 'white';
+}
+
+function htext() {
+    var x = document.getElementById("div1");
+    var y = document.getElementById("div2");
+    x.style.color = 'white';
+    y.style.color = 'yellow';
+}
